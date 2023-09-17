fc7685ad8b6c0bcb68f2d8a08b041f256e1ed849
Devan Anjelito <anjelitodevan@gmail.com>
2023-09-17 19:34:46 +0700

    Create clean raw data.

--- src/config.py
+++ src/config.py
@@ -1,1 +1,1 @@
-RAW_DATA_PATH = "/home/anj/Documents/freelance doc/2023-09-01 req fariz pemilu/input/Pemilu DPRD 2019.xlsx"
+RAW_DATA_PATH = "/home/anj/Documents/freelance doc/2023-09-01 req fariz pemilu/pemilu-dki-2019/input/Pemilu DPRD 2019.xlsx"
